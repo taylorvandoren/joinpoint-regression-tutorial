85ae2d34a0553dca273691afe8b05b59363ea18d
taylorvandoren <78322036+taylorvandoren@users.noreply.github.com>
2021-10-18 20:10:50 -0500

    change file type
    change file type

---
+++ Joinpoint Program Analyses.R
@@ -0,0 +1,6 @@
+### JOINPOINT REGRESSION TUTORIAL
+### Guidelines for analyses in the Joinpoint Regression Program, built by the National Institutes of Health
+
+# Taylor P. van Doren
+# October 18, 2021
+
